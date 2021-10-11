7882de103603ee87179b396cd384cbce0922d28d
JennyIkaMaharani <71628712+JennyIkaMaharani@users.noreply.github.com>
2021-10-11 19:10:22 +0700

    Project3

---
+++ Project 3/ProjectLuasBangunKompleks/src/projectluasbangunkompleks/Lingkaran.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks;
+
+/**
+ *
+ * @author user
+ */
+public class Lingkaran {
+    double hitungLuas(int r){
+        double luas;
+        luas = 22/7 * r * r;
+        return luas;
+    }
+}
